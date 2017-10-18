5fbb2c88df3a95f9d061fe6cd9a43027d6cbda22
Cristobal Barba Gonzále <crisbarbag@gmail.com>
2017-10-18 10:16:05 +0300

    Refactoring R-NSGA-II

--- jmetal-core/src/main/java/org/uma/jmetal/operator/impl/selection/RankingAndPreferenceSelection.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/impl/selection/RankingAndPreferenceSelection.java
@@ -1,22 +1,24 @@
 package org.uma.jmetal.operator.impl.selection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import org.uma.jmetal.operator.SelectionOperator;
 import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.util.JMetalException;
 import org.uma.jmetal.util.comparator.CrowdingDistanceComparator;
+import org.uma.jmetal.util.comparator.PreferenceDistanceComparator;
 import org.uma.jmetal.util.solutionattribute.Ranking;
 import org.uma.jmetal.util.solutionattribute.impl.DominanceRanking;
 import org.uma.jmetal.util.solutionattribute.impl.PreferenceDistance;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class RankingAndPreferenceSelection<S extends Solution<?>>
         implements SelectionOperator<List<S>,List<S>> {
     private final int solutionsToSelect ;
     private List<Double> interestPoint;
     private double epsilon;
+    int rankingIndex = 0;
     /** Constructor */
     public RankingAndPreferenceSelection(int solutionsToSelect,List<Double> interestPoint,double epsilon) {
         this.solutionsToSelect = solutionsToSelect ;
@@ -39,44 +41,57 @@ public class RankingAndPreferenceSelection<S extends Solution<?>>
             throw new JMetalException("The population size ("+solutionList.size()+") is smaller than" +
                     "the solutions to selected ("+solutionsToSelect+")")  ;
         }
-
-        Ranking<S> ranking = new DominanceRanking<S>();
-        ranking.computeRanking(solutionList) ;
-
-        return preferenceDistanceSelection(ranking,solutionList.get(0).getNumberOfObjectives());
-    }
-    protected List<S> preferenceDistanceSelection(Ranking<S> ranking,int numberOfObjectives) {
+        int index=0;
+        int numberOfObjectives = solutionList.get(0).getNumberOfObjectives();
         int nInteresPoint=this.interestPoint.size()/numberOfObjectives;
+        List<S> solution = new ArrayList<>(solutionsToSelect);
+        List<S> aux = new ArrayList<>(solutionList);
+        Ranking<S> ranking = new DominanceRanking<S>();
+        ranking.computeRanking(aux);
+rankingIndex=0;
+        for(int n = 0;n<nInteresPoint;n++) {
 
-            List<S> population = new ArrayList<>(solutionsToSelect);
 
-            while (population.size() < solutionsToSelect) {
-                int indexPoint=0;
-                for(int n = 0;(n<nInteresPoint)&& (population.size() < solutionsToSelect);n++) {
-                    List<S> auxPopulation = new ArrayList<>(solutionsToSelect/nInteresPoint);
-                    List<Double> auxInterestPoint = nextInterestPoint(indexPoint,numberOfObjectives);
-                    indexPoint+=numberOfObjectives;
-                    PreferenceDistance<S> preferenceDistance = new PreferenceDistance<>(auxInterestPoint, epsilon);
-                    int rankingIndex = 0;
-                    while ((auxPopulation.size() < (solutionsToSelect/nInteresPoint))&& (population.size() < solutionsToSelect)) {
-                        if (subfrontFillsIntoThePopulation(ranking, rankingIndex, auxPopulation)) {
-                            addRankedSolutionsToPopulation(ranking, rankingIndex, auxPopulation);
-                            rankingIndex++;
-                        } else {
-                            preferenceDistance.computeDensityEstimator(ranking.getSubfront(rankingIndex));
-                            addLastRankedSolutionsToPopulation(ranking, rankingIndex, auxPopulation);
-                        }
-                    }
-                    population.addAll(auxPopulation);
+
+            List<Double> auxInterestPoint = nextInterestPoint(index, numberOfObjectives);
+            index += numberOfObjectives;
+            PreferenceDistance<S> preferenceDistance = new PreferenceDistance<>(auxInterestPoint, epsilon);
+            /*for (int i = 0; i <  ranking.getNumberOfSubfronts(); i++) {
+                preferenceDistance.computeDensityEstimator(ranking.getSubfront(i));
+                preferenceDistance.epsilonClean(ranking.getSubfront(i));
+            }*/
+           solution.addAll(preferenceDistanceSelection(ranking,solutionsToSelect/nInteresPoint,preferenceDistance));
+        }
+
+        return solution;
+    }
+    protected List<S> preferenceDistanceSelection(Ranking<S> ranking,int sizeList,PreferenceDistance<S> preferenceDistance) {
+
+
+        List<S> population = new ArrayList<>(sizeList);
+
+
+
+        while (population.size() < sizeList) {
+            preferenceDistance.computeDensityEstimator(ranking.getSubfront(rankingIndex));
+            preferenceDistance.epsilonClean(ranking.getSubfront(rankingIndex));
+
+            if (subfrontFillsIntoThePopulation(ranking, rankingIndex, population,sizeList)) {
+
+                addRankedSolutionsToPopulation(ranking, rankingIndex, population);
+                rankingIndex++;
+            } else {
+               //addLastRankedSolutionsToPopulationPreference(ranking, rankingIndex, population,preferenceDistance,sizeList);
+               addLastRankedSolutionsToPopulation(ranking, rankingIndex, population,sizeList);
+rankingIndex++;
             }
         }
-        PreferenceDistance<S> preferenceDistance  = new PreferenceDistance<>(interestPoint, epsilon);
-        population=preferenceDistance.epsilonClean(population);
+
         return population ;
     }
 
-    protected boolean subfrontFillsIntoThePopulation(Ranking<S> ranking, int rank, List<S> population) {
-        return ranking.getSubfront(rank).size() < (solutionsToSelect - population.size()) ;
+    protected boolean subfrontFillsIntoThePopulation(Ranking<S> ranking, int rank, List<S> population,int sizeList) {
+        return ranking.getSubfront(rank).size() < (sizeList - population.size()) ;
     }
 
     protected void addRankedSolutionsToPopulation(Ranking<S> ranking, int rank, List<S> population) {
@@ -89,13 +104,23 @@ public class RankingAndPreferenceSelection<S extends Solution<?>>
         }
     }
 
-    protected void addLastRankedSolutionsToPopulation(Ranking<S> ranking, int rank, List<S>population) {
+    protected void addLastRankedSolutionsToPopulation(Ranking<S> ranking, int rank, List<S>population,int size) {
         List<S> currentRankedFront = ranking.getSubfront(rank) ;
 
         Collections.sort(currentRankedFront, new CrowdingDistanceComparator<S>()) ;
-
         int i = 0 ;
-        while (population.size() < solutionsToSelect) {
+        while (population.size() < size) {
+            population.add(currentRankedFront.get(i)) ;
+            i++ ;
+        }
+    }
+    protected void addLastRankedSolutionsToPopulationPreference(Ranking<S> ranking, int rank, List<S>population,PreferenceDistance preferenceDistance,int sizeList) {
+        List<S> currentRankedFront = ranking.getSubfront(rank) ;
+
+
+         Collections.sort(currentRankedFront, new PreferenceDistanceComparator<>(preferenceDistance)) ;
+        int i = 0 ;
+        while (population.size() < sizeList) {
             population.add(currentRankedFront.get(i)) ;
             i++ ;
         }
